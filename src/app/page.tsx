46a993dbc1a3dee037ca8e888997c1d7e6b5330c
jayajayswal01 <jayajayswal49@gmail.com>
2025-08-22 20:17:25 +0530

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import style from "./page.module.css"
 import Navbar from "@/components/navbar/navbar";
 import Hero from "@/components/hero/hero";
+import HomeComponent from "@/components/home/home";
 export default function Home() {
   return (
     <div>
       <main>
         <Navbar />
         <Hero />
+        <HomeComponent />
       </main>
     </div>
   );
